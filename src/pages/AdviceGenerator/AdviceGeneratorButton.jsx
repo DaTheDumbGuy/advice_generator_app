57e76872e473dc96d58af62c9dafbf70676a8d5a
Daryl Bacurin <darylbacurin14@gmail.com>
2024-05-21 21:10:21 +0800

    Fix Button Image

--- src/pages/AdviceGenerator/AdviceGeneratorButton.jsx
+++ src/pages/AdviceGenerator/AdviceGeneratorButton.jsx
@@ -2,7 +2,7 @@ import "./adviceGeneratorButton.scss";
 export default function AdviceGeneratorButton({ handleAdviceButton }) {
   return (
     <button onClick={handleAdviceButton}>
-      <img src="src/assets/images/icon-dice.svg" alt="" />
+      <img src="/src/assets/images/icon-dice.svg" alt="" />
     </button>
   );
 }
